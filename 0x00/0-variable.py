d206a9263a54c57cac93205a291a6f3fc6d456fe
Jet-ira <jethitech@gmail.com>
2025-03-12 13:06:01 +0100

    0x00

---
+++ 0x00/0-variable.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a file that contain a variable"""
+name = "Rashnotech"
+print(name)
+message = "I love python programming"
+print(message)
---
+++ 0x00/1-datatype.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a file that contains datatype"""
+num1 = 2
+num2 = 2.5
+name = "Rashnotech"
+istrue = True
+print(type(num1))
+print(type(num2))
+print(type(istrue))
+print(type(name))
